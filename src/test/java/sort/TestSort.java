8b3d71701370fec7f557a8f1a94580a39d73ed2e
liunian <liunian@forgame.com>
2018-07-24 17:04:11 +0800

    add junit test

--- src/test/java/sort/TestSort.java
+++ src/test/java/sort/TestSort.java
@@ -1,9 +1,24 @@
 package sort;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * 测试排序类
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:ApplicationContext.xml")
 public class TestSort {
 
-    public static void main(String[] args) {
+    /**
+     * 测试冒泡排序
+     */
+    @Test
+    public void testBubbleSort() {
         Integer[] intArray = {2,4,9,1,0,3,5,8,6};
 
         SortContext sortContext = new SortContext(new BubbleSort());
